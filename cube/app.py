bae63939ffcb07cc0f135bfcdd17b20d98c50399
RikoNaka <xxy1836@gmail.com>
2025-09-04 10:35:33 +0800

    Performence fix

--- cube/app.py
+++ cube/app.py
@@ -8,6 +8,7 @@ SLEEP_TIME = 2.0
 # MAX LEN is 30
 TEMP_HISTORY = []
 TEMP_HISTORY_MAX_LEN = 30
+LAST_FAN_STATUS = 100
 
 
 def oled_job(oled: OLED, temps):
@@ -37,28 +38,34 @@ def fan_job(cube: Cube, temps):
     if nvme.current > highest_temp:
         highest_temp = nvme.currents
 
+    global TEMP_HISTORY
+    global LAST_FAN_STATUS
+
     while len(TEMP_HISTORY) > TEMP_HISTORY_MAX_LEN:
         TEMP_HISTORY.pop()
 
     TEMP_HISTORY.append(highest_temp)
 
-    if highest_temp > FAN_WORK_TEMP:
-        # open fan
-        cube.set_fan(1)
-        cube.set_rgb_effect(3)
-        cube.set_rgb_speed(1)
-        # cube.set_single_color(0, 0, 0, 0)
-    else:
-        # close fan
-        low_temp_count = 0
-        for t in TEMP_HISTORY:
-            if t < FAN_WORK_TEMP:
-                low_temp_count += 1
+    high_temp_count = 0
+    for t in TEMP_HISTORY:
+        if t > FAN_WORK_TEMP:
+            high_temp_count += 1
 
-        if low_temp_count / len(TEMP_HISTORY) > 0.8:
+    if high_temp_count / len(TEMP_HISTORY) > 0.3:
+        if LAST_FAN_STATUS != 1:
+            # open fan
+            cube.set_fan(1)
+            cube.set_rgb_effect(3)
+            cube.set_rgb_speed(1)
+            LAST_FAN_STATUS = 1
+            # cube.set_single_color(0, 0, 0, 0)
+    else:
+        if LAST_FAN_STATUS != 0:
+            # close fan
             cube.set_fan(0)
             # close the light
             cube.set_single_color(0, 0, 0, 0)
+            LAST_FAN_STATUS = 0
 
 
 def main():
